13bd5917f4232805139607e7c92e555bcbae99f2
BuildTools <unconfigured@null.spigotmc.org>
2017-07-25 15:52:28 +0200

    Falle fertig gemacht

--- Assets/scripts/Trap.cs
+++ Assets/scripts/Trap.cs
@@ -6,29 +6,22 @@ using UnityEngine.UI;
 public class Trap : MonoBehaviour {
     Vector3 hoehe;
     GameObject trap;
-    public float gravity = 1;
-    public float seconds = 1;
+    public float gravity = 2;
 	// Use this for initialization
 	void Start () {
         trap = GameObject.Find("Trap");
         hoehe = trap.transform.position;
-        hoehe.y = 7;
+        hoehe.y = 4;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        StartCoroutine(warten());
+        hoehe.y -= gravity * Time.deltaTime;
+        trap.transform.position = hoehe;
         if (trap.transform.position.y <= 1.4)
         {
-            hoehe.y = 7;
+            hoehe.y = 4;
             trap.transform.position = hoehe;
         }
 	}
-    IEnumerator warten()
-    {
-        hoehe.y -= gravity;
-        yield return new WaitForSeconds(seconds);
-        trap.transform.position = hoehe;
-        yield return new WaitForSeconds(seconds);
-    }
 }
